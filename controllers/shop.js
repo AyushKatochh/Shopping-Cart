fb10072b6668cef4a4695cd45a73c4a390ff4c70
Ayush Katoch <katochayushhp@gmail.com>
2021-08-24 20:13:22 +0530

    Update shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -8,12 +9,6 @@ exports.getProducts = (req, res, next) => {
       path: '/products'
     });
   });
-};
-
-exports.postCart = (req, res, next) => {
-    const prodId = req.body.productId;
-    console.log(prodId);
-    res.redirect('/cart')
 };
 
 exports.getProduct = (req, res, next) => {
@@ -42,6 +37,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
