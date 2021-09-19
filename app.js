63ca2e1f53aef15c95a63c6ae1e4e9a50246875f
Ayush Katoch <katochayushhp@gmail.com>
2021-09-19 15:23:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,45 +3,52 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require("express-session");
+
+// Storing cookie in a database
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =  "mongodb+srv://AyushKatoch:ayush2002@cluster0.nj2xk.mongodb.net/shop"
+  
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 
-app.use((req, res, next) => {
-  User.findById('613b9868b736640d48be4efc')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://AyushKatoch:ayush2002@cluster0.nj2xk.mongodb.net/shop?retryWrites=true&w=majority'
+    MONGODB_URI
   )
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Ayush',
+          email: 'katochayushhp@gmail.com',
           cart: {
             items: []
           }
