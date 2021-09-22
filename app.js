f7440bb61f189ab683e1780f7ab76c0dbf94cc01
Ayush Katoch <katochayushhp@gmail.com>
2021-09-22 19:09:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require("express-session");
-
-// Storing cookie in a database
-const MongoDBStore = require("connect-mongodb-session")(session);
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =  "mongodb+srv://<username>:<password>@cluster0.nj2xk.mongodb.net/shop"
-  
+const MONGODB_URI =
+  'mongodb+srv://AyushKatoch:ayush2002@cluster0.nj2xk.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
 
 app.set('view engine', 'ejs');
@@ -29,9 +27,26 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
-
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -40,22 +55,8 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    MONGODB_URI
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Ayush',
-          email: 'katochayushhp@gmail.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
